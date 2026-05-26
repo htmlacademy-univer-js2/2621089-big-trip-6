7e382b52ec058e452d3676b8ce9f6d421c6352e4
milanaBazh <milana42297@gmail.com>
2026-05-26 18:36:49 +0500

    исправления

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, '2621089-big-trip-6', 'src', 'main.js'),
+  entry: path.resolve(__dirname, 'src', 'main.js'),  // Убрал 2621089-big-trip-6
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -12,10 +11,15 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '2621089-big-trip-6', 'public', 'index.html'),
+      template: path.resolve(__dirname, 'public', 'index.html'),  // Убрал 2621089-big-trip-6
       filename: 'index.html'
     }),
-
+    // Добавьте CopyWebpackPlugin если нужен
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     { from: 'public', to: 'public' }
+    //   ]
+    // })
   ],
   module: {
     rules: [
